784988b630e27360ef32c1e5d70c150fe7a3b9ca
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-03-31 20:49:08 -0300

    Feat: create content foote

--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,56 @@
 "use client";
-
-import styles from '../styles/globals.css';
-
 import React from 'react';
-import {useRouter} from 'next/navigation';
 
-export default function Page() {
-  const router = useRouter();
+import styles from '../styles/footer.module.css';
+
+import FooterLinks from '../components/FooterLinks';
+
+
+export default function Footer() {
+
+const links = [
+  {url : '#', text: 'Home'},
+  {url : '#', text: 'Catálogo'},
+  {url : '#', text: 'Ofertas'},
+  {url : '#', text: 'Sobre Nós'},
+  {url : '#', text: 'contato'},
+];
+
+const categorias = [
+  {url : '#', text: 'Ficção Científica'},
+  {url : '#', text: 'Romance'},
+  {url : '#', text: 'Técnologia'},
+  {url : '#', text: 'Fantasia'},
+  {url : '#', text: 'História'},
+];
+const Redes = [
+  {url : '#', text: '📸 Instagram'},
+  {url : '#', text: '🎵 TikTok'},
+  {url : '#', text: '🦜 Twitter'},
+  {url : '#', text: '📼 YouTube'},
+];
+
   return (
-    <div>
+    <div className={styles.footer}>
 
-    <div className='styles.text_container'>
-      <h1 className='styles.title'> 📚 BooksVerse</h1>
-      <p className='styles.content'>Onde histórias ganham vida!</p>
-      <p className='styles.content'>Explore um universo de livros e descubra novas aventuras.Aqui, cada página é uma porta para um mundo diferente.</p>
+    <div className={styles.footer_container}>
+
+    <div className={styles.text_container}>
+      <div className={styles.brand}>
+      <h1 className={styles.logo}> 📚 Books<span className={styles.highlight}>Verse</span></h1>
+      </div>
+      <p className={styles.content}>Onde histórias ganham vida!</p>
+      <p className={styles.content}>Explore um universo de livros e descubra novas aventuras.Aqui, cada página é uma porta para um mundo diferente.</p>
     </div>
 
-   <div>
-      <h2 className='styles.subtitle'>Categorias</h2>
-        <ul className='styles.link_list'>
-          <li>
-            <a href="/fiction" className='styles.link'>Ficção</a>
-          </li>
-          <li>
-            <a href="/non-fiction" className='styles.link'>Não Ficção</a>
-          </li>
-          <li>
-            <a href="/adventure" className='styles.link'>Aventura</a>
-          </li>
-          <li>
-            <a href="/mystery" className='styles.link'>Mistério</a>
-          </li>
-        </ul>
-      
-   </div>
+    
+    <FooterLinks linkTitle="Links Rápidos" linkList={links}/>
+
+    <FooterLinks linkTitle="Categorias" linkList={categorias}/>
+
+    <FooterLinks linkTitle="Redes Sociais" linkList={Redes}/>
+
+    </div>
 
     </div>
   );
