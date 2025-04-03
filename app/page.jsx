a0042c60adabb6d599177c505eb29ea0cf8f6896
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-04-03 08:31:35 -0300

    Feat: create Header Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,17 @@
 "use client";
 
 import React from "react";
+import styles from "../styles/anuncios.module.css";
 
 import AnunciosPage from "../components/anuncios";
 import Footer from "../components/Footer";
-import styles from "../styles/anuncios.module.css";
+import Header from '../components/Header';
+
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
+      <Header />
       <AnunciosPage />
       <Footer />
     </div>
