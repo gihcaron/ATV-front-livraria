1a2f442670cdfdc4975df752b9c42c5438889f98
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-04-03 11:21:49 -0300

    Feat: create section banner novidades

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ import LancamentosPage from "../components/lancamentos";
 import Header from "../components/Header";
 import "../styles/globals.css"; 
 
+
 export default function HomePage() {
   return (
     <div>
@@ -19,10 +20,68 @@ export default function HomePage() {
           Ver ofertas
         </button>
       </main>
+      <LancamentosPage />
 
       <AnunciosPage />
       <LancamentosPage />
+
+      <section style={styles.container}>
+        <h2 style={styles.title}>Nossos Lançamentos</h2>
+        <p style={styles.subtitle}>Não sei o que sei oq lá</p>
+        <div style={styles.form}>
+          <input type="email" placeholder="Email" style={styles.input} />
+          <button style={styles.button}>Enviar</button>
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
 }
+
+const styles = {
+  container: {
+    backgroundColor: '#3c0c64',
+    padding: '20px',
+    textAlign: 'center',
+    marginTop: '20px',
+    marginBottom: '20px',
+    marginLeft: '55px',
+    marginRight: '55px',
+    height: '15rem',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '24px',
+    marginBottom: '10px',
+    marginTop: '30px',
+  },
+  subtitle: {
+    color: '#fff',
+    fontSize: '16px',
+    marginBottom: '20px',
+  },
+  form: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '10px',
+  },
+  input: {
+    padding: '10px',
+    borderRadius: '4px',
+    border: '1px solid #ccc',
+    flex: '1',
+    maxWidth: '300px',
+    backgroundColor: '#fff',
+    border: '#fff',
+  },
+  button: {
+    padding: '10px 20px',
+    backgroundColor: '#e3abfb',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
+};
